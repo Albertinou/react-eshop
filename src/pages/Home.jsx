ad193bb70a41f89096065c0c5c19c3488b035558
Albertinou <albertuciukas@gmail.com>
2022-11-15 12:41:42 +0200

    added new componnents

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,33 @@
+import Navbar from '../components/navbar/Navbar';
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <Navbar />
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        aspernatur architecto neque quia nostrum, blanditiis rerum a, odio
+        molestias unde numquam fugit pariatur voluptates magnam quidem eaque
+        odit corrupti illo enim laudantium impedit fugiat tempore. Enim, sit!
+        Placeat perferendis inventore incidunt assumenda unde quibusdam porro
+        numquam corrupti, quod optio, a natus consequuntur repudiandae quasi
+        nesciunt. Voluptatum ea libero, rerum, magnam repudiandae id labore
+        fugiat molestiae unde animi perferendis doloribus. Ratione beatae
+        commodi dolore reprehenderit facilis deserunt excepturi consectetur non
+        quia a reiciendis autem, rem eos? Nihil minima minus quos repellat
+        debitis! Hic veniam, eos fugit distinctio odio dolor repellendus tenetur
+        adipisci quo voluptatibus pariatur autem officiis eius? Officia non
+        mollitia quis sit ad, consequuntur deserunt nam molestias. Deleniti
+        debitis ullam earum! Illo iusto earum aliquid minus perspiciatis totam
+        praesentium? Ad, non necessitatibus? Nisi harum alias, inventore
+        accusamus dignissimos quod laboriosam voluptates, sequi maxime dolore
+        asperiores eum illo. Mollitia est voluptas minus veniam libero dolorum.
+        Id assumenda dolore repellat libero eius quod nemo, nobis eaque aliquid
+        quae et, reprehenderit maiores voluptatem.
+      </p>
+    </>
+  );
 }
 
 // eslint-disable-next-line prettier/prettier
