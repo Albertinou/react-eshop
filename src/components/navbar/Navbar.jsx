abd656da8fcd7d5e36a92d0398455a9067549908
Albertinou <albertuciukas@gmail.com>
2023-01-13 09:13:10 +0200

    Navbar fixes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 
 function Navbar() {
-<<<<<<< HEAD
-  return <h1>Navbar</h1>;
-=======
   return (
     <header>
       <nav>
-        <a href="/#"></a>
         <a href="/#">Home</a>
         <a href="/#">Products</a>
         <a href="/#">Sale</a>
         <a href="/#">About</a>
-        <button>Login</button>
-        <button>Login</button>
-      </nav>
+        <p>Login</p>
       </nav>
     </header>
   );
->>>>>>> c687ef94d91ddeffb7cb242064be49ebb4be1da0
 }
 
 export default Navbar;
